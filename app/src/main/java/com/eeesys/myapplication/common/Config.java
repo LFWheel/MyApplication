6757a937ceacb802ec9b386325b419ebd467e10f
zhoujing <895406705@qq.com>
2016-06-08 15:02:44 +0800

    add a common folder

---
+++ app/src/main/java/com/eeesys/myapplication/common/Config.java
@@ -0,0 +1,8 @@
+package com.eeesys.myapplication.common;
+
+/**
+ * Created by ASUS on 2016/6/8.
+ */
+public class Config {
+    public final static String BMOBKEY = "a8515328a9497576f894449a7b86817b";
+}
---
+++ app/src/main/java/com/eeesys/myapplication/common/CusApplication.java
@@ -0,0 +1,17 @@
+package com.eeesys.myapplication.common;
+
+import android.app.Application;
+
+/**
+ * Created by ASUS on 2016/6/8.
+ */
+public class CusApplication extends Application {
+    public CusApplication() {
+        super();
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
